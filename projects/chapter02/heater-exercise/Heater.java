98fba9bd3ef073c02a9f8ae6fe791c133d098978
Jouke Rienstra <you@example.com>
2016-02-12 12:09:50 +0100

    Added an increment function and checks for the increment function

--- projects/chapter02/heater-exercise/Heater.java
+++ projects/chapter02/heater-exercise/Heater.java
@@ -27,8 +27,9 @@ public class Heater
      * Increase the temperature by a certain increment
      */
     public void warmer(){
-        if(temperature == max){ //check if the temperature is equal to the maximum alllowed temperature
+        if((temperature+=increment) >= max){ //make sure the tempeature can't be higher than the maximum temperature
             System.out.println("The heater has reached it's maximum limit of temperature: " + max + " degrees");
+            temperature = max; //set the maximum temperature that is possible
         }else{
             temperature += increment;
         }
@@ -38,8 +39,9 @@ public class Heater
      * Decrease the temperature by a certain increment
      */
     public void cooler(){
-         if(temperature == min){ //check if the temperature is equal to the minimum allowed temperature
+         if((temperature-=increment) <= min){ //make sure the tempeature can't be lowed than the minimum temperature
             System.out.println("The heater has reached it's mimimum limit of temperature: " + min + " degrees");
+            temperature = min; //set the minimum temperature that is possible
         }else{
             temperature -= increment;
         }
@@ -52,4 +54,11 @@ public class Heater
         return temperature;
     }
     
+    public void setIncrement(double temperatureIncrement){
+        if(temperatureIncrement > 0){ //check if increment is not a negative number
+            increment = temperatureIncrement;
+        }else{
+            System.out.println("The increment should be higher than 0");
+        }
+    }
 }
