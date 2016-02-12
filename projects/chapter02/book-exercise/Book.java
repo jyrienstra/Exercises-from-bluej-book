c8565fb43725f1d17b39b395b47e46583a7da207
Jouke Rienstra <you@example.com>
2016-02-12 10:52:57 +0100

    returns author/title/ammount of pages, print the author/title/details of the book. Set/print a reference number of a library. Function for the ammount of time a book is borrowed set&getter

--- projects/chapter02/book-exercise/Book.java
+++ projects/chapter02/book-exercise/Book.java
@@ -11,16 +11,104 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private String refNumber;
+    private int pages;
+    private int borrowed;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
+        refNumber = "";
+        borrowed = 0;
     }
-
+    
     // Add the methods here ...
+    
+    /**
+     * Return the author of the book
+     */
+    public String getAuthor(){
+        return author;
+    }
+    
+    /**
+     * Return the title of the book
+     */
+    public String getTitle(){
+        return title;
+    }
+    
+    /**
+     * Return the ammount of pages of the book
+     */
+    public int getPages(){
+        return pages;
+    }
+    
+    /**
+     * Print the author the book
+     */
+    public void printAuthor(){
+        System.out.println("The author of te the book is " + author);
+    }
+    
+    /**
+     * Print the title of the book
+     */
+    public void printTitle(){
+        System.out.println("The title of the book is " + title);
+    }
+    
+    /**
+     * Print the details of the book
+     */
+    public void printDetails(){
+        System.out.println("Title= " + title + ", Author " + author + ", Amount of pages " + pages);
+        System.out.println("The book is borrowed " + borrowed + " times");
+        
+        if(refNumber.length() > 0){
+           System.out.println("Refnumber= " + refNumber);
+        }else{
+           System.out.println("No refnumber");
+        }
+    }
+    
+    /**
+     * Set a referencenumber for a library
+     */
+    public void setRefNumber(String ref){
+        if(ref != null && ref.length() >= 3){
+            refNumber = ref;
+        }else{
+            System.out.println("Refnumber must be equal or bigger than 3 and can't be null");
+        }
+    }
+    
+    /**
+     * Print the referencenumber of a library
+     */
+    public String getRefNumber(){
+        return refNumber;
+    }
+    
+    /**
+     * Return the ammount of the times the book is borrowed
+     */
+     public int getBorrowed(){
+        return borrowed;
+    }
+    
+    /**
+     * Set the borrowed +1 
+     */
+    public void setBorrowed(){
+        borrowed += 1;
+    }
+    
 }
